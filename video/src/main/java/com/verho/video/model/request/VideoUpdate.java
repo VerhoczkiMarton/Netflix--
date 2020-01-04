5d28a5a7fa00bc79b40853757249f07d1a0da2f6
Verhóczki Márton <verhoczki.marci@gmail.com>
2020-01-04 13:04:10 +0100

    Add annotation to VideoUpdate.java to ignore null values

--- video/src/main/java/com/verho/video/model/request/VideoUpdate.java
+++ video/src/main/java/com/verho/video/model/request/VideoUpdate.java
@@ -1,10 +1,12 @@
 package com.verho.video.model.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class VideoUpdate {
     private String name;
     private String url;
